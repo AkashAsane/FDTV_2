1bb20b5af58f9589a75892a4c064c417ed0e526f
AkashAsane <akashasane4455@gmail.com>
2024-05-10 11:37:26 +0530

    Test commit

--- stopwatch/src/App.js
+++ stopwatch/src/App.js
@@ -3,11 +3,56 @@ import { useEffect,useState } from 'react';
 import axios from 'axios';
 import './App.css';
 
-function App() {
+function Stopwatch() {
+
+    const[time,setTime]=useState(0);
+    const[isrunning,setisrunning]=useState(false);
+
+    useEffect(()=>{
+          let interval
+          if(isrunning)
+          {
+             interval=setInterval(()=>{
+              setTime(prev=>prev+1)
+            },1000)
+          }
+
+          return ()=>clearInterval(interval)
+    },[isrunning]);
+
+     
+      const  handlestart=()=>{
+        setisrunning(true)
+      }
+
+      const handlestop=()=>{
+        setisrunning(false);
+      }
+
+      const handlereset=()=>{
+        setTime(0);
+        setisrunning(false)
+      }
+      
+      const createTime=(timesec)=>{
+         const min= Math.floor(timesec/60);
+         const sec= timesec%60;
+         return `${min}:${sec<10 ? "0" : ""} ${sec}`;
+      }
 
     return(
-      <div>hello</div>
+      <>
+         <h1>Stopwatch</h1>
+         <h3>{createTime(time)}</h3>
+          { !isrunning ?(
+            <button onClick={handlestart}>Start</button>
+          ):(
+            <button onClick={handlestop}>Stop</button>
+          )}
+          <button onClick={handlereset}>reset</button>
+          
+      </>
     )
 }
 
-export default App;
+export default Stopwatch;
