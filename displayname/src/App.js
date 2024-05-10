2a5967c8c2aa108ca955bc3aeea87750bef775e4
AkashAsane <akashasane4455@gmail.com>
2024-05-10 16:34:34 +0530

    test commit

---
+++ displayname/src/App.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+
+function  Displayname() {
+    const[name,setName]=useState("");
+    const[lastname,setLastname]=useState("");
+    const[getfirst,setgetfirst]=useState("");
+    const[getlast,setgetlast]=useState("");
+    const[issubmitted,setIssubmitted]=useState(false);
+
+
+    const  handlesubmit=(event)=>{
+        event.preventDefault();
+         if(name.length !==0)
+         {
+               setgetfirst(name);
+               setgetlast(lastname);
+               setIssubmitted(true);
+         }
+         
+    }
+         
+
+    return (
+        <div>
+            <h2>Full Name Display</h2>
+            <form onSubmit={handlesubmit}>
+                <div>
+                <label>First name :</label>
+                <input
+                  type='text'
+                  value={name}
+                  onChange={(e)=>setName(e.target.value)}
+                  required
+                />
+                </div>
+                <div>
+                <label>Last name :</label>
+                <input
+                  type='text'
+                  value={lastname}
+                  onChange={(e)=>setLastname(e.target.value)}
+                  required
+                />
+                </div>
+                <button type='submit'>Submit</button>
+            </form>
+
+            <div>
+                {issubmitted &&(
+                    <h4>Full Name:{getfirst} {getlast}</h4>
+                )
+                    
+                }
+                
+            </div>
+        </div>
+    );
+}
+
+export default Displayname;
