c70215b816b244e4c9c1302aaccae2dfc544426f
AkashAsane <akashasane4455@gmail.com>
2024-05-10 21:35:44 +0530

    test commit

--- stopwatch/src/index.js
+++ stopwatch/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Counter from './App';
+import Stopwatch from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Counter/>
+    <Stopwatch/>
   </React.StrictMode>
 );
 
